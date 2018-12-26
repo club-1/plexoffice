fac7429f58cfcecf73a399b209563881b5ebe67a
n-peugnet <n.peugnet@free.fr>
2018-12-26 13:13:34 +0100

    refactor: renamed plex1 into plexoffice

--- plexoffice/wsgi.py
+++ plexoffice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for plex1 project.
+WSGI config for plexoffice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plex1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plexoffice.settings')
 
 application = get_wsgi_application()
---
+++ static/collect/README.md
@@ -0,0 +1,4 @@
+# static/collect
+
+this is the folder where the `manage.y collectstatic` command should gather the
+static files
