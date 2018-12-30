31e5a31d712826790731c3fd43cc4fc09bf6a265
n-peugnet <n.peugnet@free.fr>
2018-12-30 01:11:25 +0100

    style(invite): cosmetic changes

--- invite/urls.py
+++ invite/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('token/<str:invitationToken>', views.home, name='invite-home'),
+    path('token/<str:invitationToken>', views.invite, name='plex-invite'),
     path('add', views.addFriend, name='invite-add'),
     path('confirm', views.confirm, name='invite-confirm'),
     path('expired', views.expired, name='invite-expired'),
--- invite/views.py
+++ invite/views.py
@@ -9,13 +9,13 @@ from .forms import addForm
 from .models import Invitation
 
 
-def home(request, invitationToken=''):
+def invite(request, invitationToken=''):
     form = addForm(None)
     invitation = get_object_or_404(Invitation, token=invitationToken)
     if invitation.date_usage != None:
         return redirect('invite-expired')
     form.fields['invitation'].initial = invitationToken
-    return render(request, 'invite/home.html', {'form': form})
+    return render(request, 'invite/invite.html', {'form': form})
 
 
 def confirm(request):
@@ -53,10 +53,11 @@ def addFriend(request):
             invitation.used_by = user.email
             invitation.save()
             return redirect('invite-confirm')
-        except BadRequest:
+        except BadRequest as e:
+            print(e)
             messages.error(
                 request,
                 'L\'accès a déjà été accordé à cet utilisateur')
         except Exception as e:
             messages.error(request, 'Internal error: ' + e)
-    return redirect('invite-home', invitationToken)
+    return redirect('plex-invite', invitationToken)
